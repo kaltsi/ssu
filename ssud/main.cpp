94d1d0e85c138fe702a9384ab9d6d9f78d1edf32
Juha Kallioinen <juha.kallioinen@jolla.com>
2014-02-24 18:06:33 +0200

    wip

--- ssud/main.cpp
+++ ssud/main.cpp
@@ -9,7 +9,7 @@
 #include <QTranslator>
 #include <QLocale>
 #include <QLibraryInfo>
-#include <connman-qt5/connmannetworkproxyfactory.h>
+// #include <connman-qt5/connmannetworkproxyfactory.h>
 #include "ssud.h"
 
 int main(int argc, char** argv){
@@ -23,7 +23,7 @@ int main(int argc, char** argv){
                     QLibraryInfo::location(QLibraryInfo::TranslationsPath));
   app.installTranslator(&qtTranslator);
 
-  QNetworkProxyFactory::setApplicationProxyFactory(new ConnmanNetworkProxyFactory);
+  //  QNetworkProxyFactory::setApplicationProxyFactory(new ConnmanNetworkProxyFactory);
 
   Ssud ssud;
 
